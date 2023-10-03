dceec46e87ce9395dbcbb22ddd7e79c4ccfec9b0
Evgeny Grakovich <grakovich-e@yandex.by>
2023-10-03 13:35:47 +0300

    initial commit

---
+++ bot/main_psy_conf.py
@@ -0,0 +1,7 @@
+from loader import dp
+from aiogram import executor
+from handlers import *
+
+if __name__ == '__main__':
+    print("Starting Flats Renting Bot 2.0")
+    executor.start_polling(dp, skip_updates=True)
