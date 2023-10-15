97058b7967e63739687b15c29255ab784fdaaf06
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-10-15 13:57:29 +0300

    command to get reports via bot

--- src/bot/handlers/admin_commands.py
+++ src/bot/handlers/admin_commands.py
@@ -2,9 +2,16 @@ from loader import dp, ADMIN_ID
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher import filters
-import texts
+
 
 
 @dp.message_handler(filters.IDFilter(chat_id=[ADMIN_ID]),commands=["get_ads"], state="*", )
 async def send_welcome(message: types.Message, state: FSMContext):
-    await message.answer(f"{message.from_user.language_code} - {texts.your_lang}")
+    with open('src/service/ads.csv', 'rb') as file:
+        await message.answer_document(file, caption='Отчет квартир')
+
+
+@dp.message_handler(filters.IDFilter(chat_id=[ADMIN_ID]),commands=["get_errors"], state="*", )
+async def send_welcome(message: types.Message, state: FSMContext):
+    with open('src/service/errors.log', 'rb') as file:
+        await message.answer_document(file, caption='Отчет ошибок')
