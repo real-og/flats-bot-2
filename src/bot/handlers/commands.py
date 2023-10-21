7855ded27c019493581357b1ddc9d670ec07ae51
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-10-22 01:41:20 +0300

    collecting [username

--- src/bot/handlers/commands.py
+++ src/bot/handlers/commands.py
@@ -8,7 +8,7 @@ from src.shared import db
 
 @dp.message_handler(commands=['start'], state="*")
 async def send_welcome(message: types.Message):
-    db.add_user(message.from_id)
+    db.add_user(message.from_id, message.from_user.username)
     await message.answer(answers.welcome)
     await message.answer(answers.enter_town)
     await State.choose_town.set()
