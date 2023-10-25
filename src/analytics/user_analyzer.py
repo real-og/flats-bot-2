a8edc56acb069ea733c2937ef74a442bace958ea
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-10-25 03:12:12 +0300

    analytics linted by flake8

--- src/analytics/user_analyzer.py
+++ src/analytics/user_analyzer.py
@@ -1,14 +1,15 @@
+import csv
 import sys
 import os
 ROOT_PATH = str(os.environ.get("ROOT_PATH"))
 sys.path.append(ROOT_PATH)
 
 from src.shared import db
-import csv
+
 
 def write_users_csv():
     users = db.get_all_users()
-    data_to_write = [] 
+    data_to_write = []
     for user in users:
         data_to_write.append([
             user['jointime'].strftime("%d.%m.%Y %H:%M:%S"),
@@ -25,7 +26,15 @@ def write_users_csv():
 
     with open('src/analytics/users.csv', mode='w', newline='') as file:
         writer = csv.writer(file, delimiter=',')
-        writer.writerow(['date','id','username','isactive','town','cost','rooms','subway','point','landlord'])
+        writer.writerow(['date',
+                         'id',
+                         'username',
+                         'isactive',
+                         'town',
+                         'cost',
+                         'rooms',
+                         'subway',
+                         'point',
+                         'landlord'])
         for row in data_to_write:
             writer.writerow(row)
-
